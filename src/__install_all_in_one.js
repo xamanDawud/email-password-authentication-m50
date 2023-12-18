6c87dd4af4bbb2ee14eaa56a721962565f709267
A K Zaman Daud <106136246+xamanDawud@users.noreply.github.com>
2023-12-18 16:31:11 +0600

    express server hello world example code

--- src/__install_all_in_one.js
+++ src/__install_all_in_one.js
@@ -22,3 +22,23 @@ plugins: [require("daisyui")],
 @tailwind components;
 @tailwind utilities;
 ------------------------------
+
+
+------------- server- index.js-----------------
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
